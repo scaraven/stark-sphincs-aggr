32d730881d9fd84eb783fa80900273afe8240742
scaraven <sc4raven@gmail.com>
2026-01-23 18:03:24 +0000

    feat: simply python scripts dir

--- scripts/hash.py
+++ scripts/hash.py
@@ -6,3 +6,6 @@ def poseidon_hash(inputs: List[int]) -> int:
 
 # export
 __all__ = ["poseidon_hash"]
+
+# Sanity check for poseidon hasher
+assert(poseidon_hash([1,2,3,4,5]) == 611250879885582549814822745980582240134120981459161846704834910080944450980)
--- scripts/tests/test_poseidon.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from poseidon_hash.hash  import poseidon_hash
-
-def test_poseidon_hash_sample():
-    inputs = [1, 2, 3, 4, 5]
-    result = poseidon_hash(inputs)
-
-    expected = 611250879885582549814822745980582240134120981459161846704834910080944450980
-    assert result == expected
